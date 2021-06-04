ae6687a50e97fc4a843990827e5170d65d66febd
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-06-04 10:51:38 +0530

    [WIP] sm8150-common: New ALS correction algorithm
    * Needs brightness overlay commit in device tree
    
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>

--- sensors/AlsCorrection.cpp
+++ sensors/AlsCorrection.cpp
@@ -18,6 +18,7 @@
 
 #include <cutils/properties.h>
 #include <fstream>
+#include <cmath>
 #include <log/log.h>
 #include <time.h>
 
@@ -27,8 +28,11 @@ namespace sensors {
 namespace V2_1 {
 namespace implementation {
 
-static int red_max_lux, green_max_lux, blue_max_lux, white_max_lux, max_brightness;
-static int als_bias;
+static int red_max_lux, green_max_lux, blue_max_lux, white_max_lux, max_brightness, cali_coe;
+static int als_bias, max_lux;
+static bool als_change, first_run, recalc_next;
+static float als_change_light_diff, als_change_light_diff_local, als_change_light, prev_color_adj_light, als_change_frac;
+static float als_color_change_light_offset, max_color_adj_light, min_color_adj_light, prev_brightness, prev_color_correction, als_color_change_light, als_color_change_light_offset_local;
 
 template <typename T>
 static T get(const std::string& path, const T& def) {
@@ -45,8 +49,24 @@ void AlsCorrection::init() {
     blue_max_lux = get("/mnt/vendor/persist/engineermode/blue_max_lux", 0);
     white_max_lux = get("/mnt/vendor/persist/engineermode/white_max_lux", 0);
     als_bias = get("/mnt/vendor/persist/engineermode/als_bias", 0);
+    cali_coe = get("/mnt/vendor/persist/engineermode/cali_coe", 1000);
     max_brightness = get("/sys/class/backlight/panel0-backlight/max_brightness", 255);
-    ALOGV("max r = %d, max g = %d, max b = %d", red_max_lux, green_max_lux, blue_max_lux);
+    max_lux = red_max_lux + green_max_lux + blue_max_lux + white_max_lux;
+    ALOGD("max r = %d, max g = %d, max b = %d, max_white: %d, cali_coe: %d, als_bias: %d, max_brightness: %d, max_lux: %d", red_max_lux, green_max_lux, blue_max_lux, white_max_lux, cali_coe, als_bias, max_brightness, max_lux);
+    als_change_light_diff = 0.0f;
+    als_change_light_diff_local = 0.0f;
+    als_change_light = 0.0f;
+    prev_color_adj_light = 0.0f;
+    prev_brightness = 0.0f;
+    als_change_frac = 0.1f;
+    prev_color_correction = 0.0f;
+    als_color_change_light_offset = 0.0f;
+    als_color_change_light_offset_local = 0.0f;
+    als_color_change_light = 0.0f;
+    max_color_adj_light = 0.0f;
+    min_color_adj_light = 0.0f;
+    als_change = false;
+    first_run = true;
 }
 
 void AlsCorrection::correct(float& light) {
@@ -54,34 +74,106 @@ void AlsCorrection::correct(float& light) {
     if (pid != 0) {
         kill(pid, SIGUSR1);
     }
+    uint8_t updated = property_get_int32("vendor.sensors.als_correction.updated", 0);
+    if (updated == 0) {
+        light = (prev_color_adj_light - als_change_light_diff_local);
+        return;
+    }
     uint8_t r = property_get_int32("vendor.sensors.als_correction.r", 0);
     uint8_t g = property_get_int32("vendor.sensors.als_correction.g", 0);
     uint8_t b = property_get_int32("vendor.sensors.als_correction.b", 0);
     ALOGV("Screen Color Above Sensor: %d, %d, %d", r, g, b);
     ALOGV("Original reading: %f", light);
     int screen_brightness = get("/sys/class/backlight/panel0-backlight/brightness", 0);
-    float correction = 0.0f;
-    if (red_max_lux > 0 && green_max_lux > 0 && blue_max_lux > 0 && white_max_lux > 0) {
-        uint8_t rgb_min = std::min({r, g, b});
-        correction += ((float) rgb_min) / 255.0f * ((float) white_max_lux);
-        r -= rgb_min;
-        g -= rgb_min;
-        b -= rgb_min;
-        correction += ((float) r) / 255.0f * ((float) red_max_lux);
-        correction += ((float) g) / 255.0f * ((float) green_max_lux);
-        correction += ((float) b) / 255.0f * ((float) blue_max_lux);
-        correction = correction * (((float) screen_brightness) / ((float) max_brightness));
-        correction += als_bias;
+    if (screen_brightness == 0){
+        ALOGD("brightness is 0");
+        light = (prev_color_adj_light - als_change_light_diff_local);
+        return;
     }
-    // Do not apply correction if < 0, prevent unstable adaptive brightness
-    if (light - correction >= 0) {
-        light -= correction;
-    } else {
-        // In low light conditions, sensor is just reporting bad values, using
-        // computed correction instead allows to fix the issue
-        light = correction;
+    float color_correction = 0.0f;
+    float brightness_factor = 0.0f;
+    float brightness = 0.0f;
+    uint32_t rgb_min = 0;
+    float light_frac = 0.0f;
+    float coe_frac = ((float) cali_coe) / 1000.0;
+    float change_threshold = 0.0f;
+    float change = 0.0f;
+    if (max_lux > 0) {
+        brightness = ((float) screen_brightness) / ((float) max_brightness);
+        light_frac = (light > max_lux) ? 1.0f : light/((float) max_lux);
+        rgb_min = std::min({r, g, b});
+        color_correction += ((float) rgb_min) / 255.0f * ((float) white_max_lux);
+        color_correction += ((float) r) / 255.0f * ((float) red_max_lux);
+        color_correction += ((float) g) / 255.0f * ((float) green_max_lux);
+        color_correction += ((float) b) / 255.0f * ((float) blue_max_lux);
+        color_correction += als_bias;
+        color_correction = color_correction/max_lux;
+        light_frac = als_change ? light_frac * coe_frac/2 + light_frac * std::pow(color_correction, 0.1f) : light_frac * std::pow(color_correction, 0.8f);
+        float exp = als_change ? 1.2f - 1.2f*std::pow(color_correction*brightness, 0.1f) : 1.2f - std::pow(color_correction*brightness, 0.5f);
+        brightness_factor = std::pow(brightness, exp);
+        brightness_factor = (brightness_factor > 1.0f) ? 1.0f : brightness_factor;
+        light_frac = light_frac * brightness_factor;
+        light_frac = (light > max_lux) ? light_frac - (light - max_lux)/((float) max_lux) : light_frac;
+        light_frac = (light_frac > 0.5f) ? 0.5f : light_frac < 0.0f ? 0.0f : light_frac;
+        float color_adj_light = light * (1.0f - light_frac);
+        if (als_change) {
+            max_color_adj_light = std::max({max_color_adj_light, color_adj_light});
+            min_color_adj_light = std::min({min_color_adj_light, color_adj_light});
+            if (abs(color_correction - prev_color_correction) > 0.01f/std::pow(brightness, 0.45f)) {
+                float tmp = color_correction > prev_color_correction ? color_adj_light - min_color_adj_light : color_adj_light - max_color_adj_light;
+                als_color_change_light_offset += tmp;//abs(tmp) > color_adj_light ? color_adj_light : tmp;
+                als_color_change_light = color_adj_light;
+                max_color_adj_light = 0.0f;
+                min_color_adj_light = color_adj_light;
+            }
+        } else {
+            als_color_change_light_offset = 0.0f;
+        }
+        als_color_change_light_offset_local = (color_adj_light > als_color_change_light) ? als_color_change_light_offset : als_color_change_light_offset * color_adj_light/als_color_change_light;
+
+        light_frac = (color_adj_light > max_lux) ? 1.0f : color_adj_light/((float) max_lux);
+        change_threshold = 0.042f/std::pow(light_frac, 0.6f);
+        change = als_change ? (prev_color_adj_light-color_adj_light)/color_adj_light : (color_adj_light - prev_color_adj_light)/prev_color_adj_light;
+        if (!first_run && ((!als_change && brightness > 0.4f) || recalc_next || (!als_change && color_adj_light < max_lux && color_adj_light > prev_color_adj_light && change > change_threshold && brightness > prev_brightness ))) {
+            if (color_correction > 0.1f && !recalc_next && !als_change) {
+                recalc_next = true;
+                min_color_adj_light = color_adj_light;
+                als_change_light = color_adj_light;
+                als_change_light_diff = color_adj_light - prev_color_adj_light;
+            } else if (recalc_next) {
+                min_color_adj_light = color_adj_light;
+                als_change_light = color_adj_light;
+                als_change_light_diff += color_adj_light - prev_color_adj_light;
+                recalc_next = false;
+            } else {
+                min_color_adj_light = color_adj_light;
+                als_change_light = color_adj_light;
+                als_change_light_diff = color_adj_light - prev_color_adj_light;
+            }
+            als_change_frac = brightness;
+            als_change = true;
+        } else if (als_change && (color_adj_light < 60 || (color_adj_light < prev_color_adj_light && change > change_threshold && brightness < prev_brightness ))) {
+            als_change = false;
+            als_change_light_diff = 0.0f;
+            als_color_change_light_offset_local = 0.0f;
+            max_color_adj_light = 0.0f;
+            min_color_adj_light = 0.0f;
+        }
+        first_run = false;
+        als_change_light_diff_local = (brightness > als_change_frac) ? als_change_light_diff : als_change_light_diff * std::pow(brightness/als_change_frac, 0.25f);
+        als_change_light_diff_local += als_color_change_light_offset_local;
+        als_change_light_diff_local = abs(als_change_light_diff_local) > color_adj_light*0.95f ? als_change_light_diff_local < 0 ? -color_adj_light*0.95f : color_adj_light*0.95f : als_change_light_diff_local > 4000 ? 4000 : als_change_light_diff_local;
+        als_change_light_diff_local = (als_change_light_diff_local < 0) ? std::max({-0.1f*color_adj_light, als_change_light_diff_local}) : als_change_light_diff_local;
+        if (color_adj_light - als_change_light_diff_local > light) {
+            als_change_light_diff_local = 0;
+        }
+        prev_color_adj_light = color_adj_light;
+        prev_brightness = brightness;
+        prev_color_correction = color_correction;
     }
-    ALOGV("Corrected reading: %f", light);
+
+    ALOGD("Raw: %f Color: %f Corrected: %f correction: %f brightness: %d als_change_light: %f als_change_light_local: %f change: %f change_threshold: %f als_: %d", light, prev_color_adj_light, (prev_color_adj_light - als_change_light_diff_local), color_correction, screen_brightness, als_change_light_diff, als_change_light_diff_local, change, change_threshold, als_change ? 1:0);
+    light = (prev_color_adj_light - als_change_light_diff_local);
 }
 
 }  // namespace implementation
